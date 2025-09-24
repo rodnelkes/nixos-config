d441f42abe6dcc74d8cbd01312cfbf730e3fe26d
Zayen Yusuf <rodnelkes@gmail.com>
2025-09-24 23:39:55 +0000

    Migrate existing catppuccin options to input

--- modules/shells/nushell.nix
+++ modules/shells/nushell.nix
@@ -1,11 +1,3 @@
-let
-  nushellCatppuccin = builtins.readFile (
-    builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/nushell/05987d258cb765a881ee1f2f2b65276c8b379658/themes/catppuccin_mocha.nu";
-      sha256 = "d639441cd3b4afe1d05157da64c0564c160ce843182dfe9043f76d56ef2c9cdf";
-    }
-  );
-in
 {
   flake.modules = {
     nixos.nushell =
@@ -17,7 +9,7 @@ in
     homeManager.nushell = {
       programs.nushell = {
         enable = true;
-        extraConfig = nushellCatppuccin + (builtins.readFile ./config.toml);
+        extraConfig = builtins.readFile ./config.toml;
       };
     };
   };
--- modules/tools/catppuccin.nix
+++ modules/tools/catppuccin.nix
@@ -2,18 +2,33 @@
   inputs,
   ...
 }:
+let
+  catppuccin = {
+    flavor = "mocha";
+    accent = "rosewater";
+  };
+in
 {
   flake.modules = {
     nixos.catppuccin = {
       imports = [
         inputs.catppuccin.nixosModules.catppuccin
       ];
+
+      inherit catppuccin;
     };
 
     homeManager.catppuccin = {
       imports = [
         inputs.catppuccin.homeModules.catppuccin
       ];
+
+      catppuccin = {
+        inherit (catppuccin) flavor accent;
+
+        starship.enable = true;
+        nushell.enable = true;
+      };
     };
   };
 }
