29d928c6e152d412f5d6b66b4170e467bd58e82d
Zayen Yusuf <rodnelkes@gmail.com>
2025-09-24 05:42:24 +0000

    Use home-manager openssh service instead

--- modules/tools/ssh.nix
+++ modules/tools/ssh.nix
@@ -1,35 +1,31 @@
 {
-  flake.modules = {
-    nixos.ssh = {
-      services.openssh.enable = true;
-    };
+  flake.modules.homeManager.ssh = {
+    programs.ssh = {
+      enable = true;
 
-    homeManager.ssh = {
-      programs.ssh = {
-        enable = true;
+      # Deprecated, will be removed
+      enableDefaultConfig = false;
 
-        # Deprecated, will be removed
-        enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
 
-        matchBlocks = {
-          "*" = {
-            forwardAgent = false;
-            addKeysToAgent = "no";
-            compression = false;
-            serverAliveInterval = 0;
-            serverAliveCountMax = 3;
-            hashKnownHosts = false;
-            userKnownHostsFile = "~/.ssh/known_hosts";
-            controlMaster = "no";
-            controlPath = "~/.ssh/master-%r@%n:%p";
-            controlPersist = "no";
-          };
-
-          "github.com" = {
-            identityFile = "~/.ssh/github";
-          };
+        "github.com" = {
+          identityFile = "~/.ssh/github";
         };
       };
     };
+
+    services.ssh-agent.enable = true;
   };
 }
