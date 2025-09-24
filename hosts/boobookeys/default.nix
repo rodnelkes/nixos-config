2506bd8cf38dc3a8054362cb3f300bdec85b294e
Zayen Yusuf <rodnelkes@gmail.com>
2025-09-24 23:13:21 +0000

    Add and enable catppuccin as input

--- hosts/boobookeys/default.nix
+++ hosts/boobookeys/default.nix
@@ -14,6 +14,7 @@ let
     lix
     wsl
     home-manager
+    catppuccin
     nushell
   ];
 
@@ -21,6 +22,7 @@ let
     rodnelkes
     home-manager
     nh
+    catppuccin
     nushell
     starship
     git
