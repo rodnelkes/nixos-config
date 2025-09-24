3a013ac2c6229b03ff38622c11ed846c2cb9cb11
Zayen Yusuf <rodnelkes@gmail.com>
2025-09-24 23:39:55 +0000

    Migrate existing catppuccin options to input

--- modules/prompts/starship.nix
+++ modules/prompts/starship.nix
@@ -5,19 +5,13 @@ let
       sha256 = "4957188eeb0018d68f3fa77e1a53a8134263e221cc2db02a575bcedf85c97403";
     }
   );
-  jetpackCatppuccin = builtins.readFile (
-    builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/starship/5906cc369dd8207e063c0e6e2d27bd0c0b567cb8/themes/mocha.toml";
-      sha256 = "712699ad27db93deddda4577ab9753f76e0c82652e63c7982085343c8ca01f9c";
-    }
-  );
 in
 {
   flake.modules.homeManager.starship = {
     programs.starship = {
       enable = true;
 
-      settings = (builtins.fromTOML jetpackConfig) // (builtins.fromTOML jetpackCatppuccin);
+      settings = builtins.fromTOML jetpackConfig;
     };
   };
 }
