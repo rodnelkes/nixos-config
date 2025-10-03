009c98d8e55b062b6207212d359b88c832ffb3fd
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-03 21:43:20 +0000

    Migrate to nvim 0.11 lsp

---
+++ modules/editors/neovim/nelvim/lua/nelvim/core/lsp.lua
@@ -0,0 +1,11 @@
+vim.lsp.config("*", {
+	root_markers = { ".git", ".jj" },
+})
+
+vim.lsp.enable({ "lua_ls", "nixd" })
+
+vim.diagnostic.config({
+	virtual_lines = true,
+	update_in_insert = true,
+	severity_sort = true,
+})
