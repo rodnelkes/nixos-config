bdf4407f2bd975f7cfbb137a2c789a6593603e99
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-17 02:11:14 +0000

    nixos: Enable system-wide ssh

--- modules/tools/ssh.nix
+++ modules/tools/ssh.nix
@@ -6,39 +6,44 @@ let
   homeConfig = config.flake.homeConfigurations.rodnelkes.config;
 in
 {
-  flake.modules.homeManager.ssh = {
-    programs = {
-      ssh = {
-        enable = true;
-
-        # Deprecated, will be removed
-        enableDefaultConfig = false;
-
-        matchBlocks = {
-          "*" = {
-            forwardAgent = false;
-            addKeysToAgent = "no";
-            compression = false;
-            serverAliveInterval = 0;
-            serverAliveCountMax = 3;
-            hashKnownHosts = false;
-            userKnownHostsFile = "~/.ssh/known_hosts";
-            controlMaster = "no";
-            controlPath = "~/.ssh/master-%r@%n:%p";
-            controlPersist = "no";
-          };
-
-          "git" = {
-            host = "github.com gitlab.com";
-            identitiesOnly = true;
-            identityFile = homeConfig.sops.secrets.github.path;
-          };
-        };
-      };
-
-      nushell.extraConfig = builtins.readFile ./eval_ssh-agent.nu;
+  flake.modules = {
+    nixos.ssh = {
+      programs.ssh.startAgent = true;
+      services.openssh.enable = true;
     };
 
-    services.ssh-agent.enable = true;
+    homeManager.ssh = {
+      programs = {
+        ssh = {
+          enable = true;
+
+          # Deprecated, will be removed
+          enableDefaultConfig = false;
+
+          matchBlocks = {
+            "*" = {
+              forwardAgent = false;
+              addKeysToAgent = "no";
+              compression = false;
+              serverAliveInterval = 0;
+              serverAliveCountMax = 3;
+              hashKnownHosts = false;
+              userKnownHostsFile = "~/.ssh/known_hosts";
+              controlMaster = "no";
+              controlPath = "~/.ssh/master-%r@%n:%p";
+              controlPersist = "no";
+            };
+
+            "git" = {
+              host = "github.com gitlab.com";
+              identitiesOnly = true;
+              identityFile = homeConfig.sops.secrets.github.path;
+            };
+          };
+        };
+
+        nushell.extraConfig = builtins.readFile ./eval_ssh-agent.nu;
+      };
+    };
   };
 }
