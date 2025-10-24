37a655bdd974a5afef4f3b770b2ca8e7a5283f76
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-23 20:14:35 -0400

    lix: Use correct version

--- system/core/lix.nix
+++ system/core/lix.nix
@@ -1,16 +1,30 @@
 { sources, ... }:
 
 let
-  inherit (builtins) substring;
+  inherit (builtins) fetchGit substring;
+
+  # This is only needed because (import sources.lix).lastModifiedDate is "19700101000000"
+  # Not sure why, maybe fault lies with npins?
+  lixInfo =
+    if (import sources.lix).lastModifiedDate == "19700101000000" then
+      let
+        lixRepo = sources.lix.repository;
+        lixUrl = "${lixRepo.server}/${lixRepo.owner}/${lixRepo.repo}";
+      in
+      fetchGit {
+        url = lixUrl;
+        ref = sources.lix.branch;
+        rev = sources.lix.revision;
+      }
+    else
+      (import sources.lix) // { shortRev = substring 0 7 sources.lix.revision; };
 in
 {
   imports = [
     (import "${sources.lix-nixos-module}/module.nix" {
       lix = sources.lix.outPath;
 
-      versionSuffix = "pre${substring 0 8 (import sources.lix).lastModifiedDate}-${
-        substring 0 7 sources.lix.revision
-      }";
+      versionSuffix = "pre${substring 0 8 lixInfo.lastModifiedDate}-${lixInfo.shortRev}";
     })
   ];
 }
