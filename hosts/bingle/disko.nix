dcd0572106fbd9e16abfe98a5ab3818e25122d97
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-26 05:20:49 -0400

    bingle: Migrate to disko

---
+++ hosts/bingle/disko.nix
@@ -0,0 +1,65 @@
+{ sources, lib, ... }:
+
+let
+  inherit (builtins) map;
+  inherit (lib) mergeAttrsList recursiveUpdate;
+
+  mkBtrfsSubvolume = name: {
+    ${name} = {
+      mountpoint = "/${if name == "root" then "" else name}";
+      mountOptions = [
+        "compress=zstd"
+        "noatime"
+      ];
+    };
+  };
+
+  btrfsSubvolumes = mergeAttrsList (
+    map mkBtrfsSubvolume [
+      "root"
+      "home"
+      "nix"
+    ]
+  );
+in
+{
+  imports = [ (import "${sources.disko}/module.nix") ];
+
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              name = "boot";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = recursiveUpdate btrfsSubvolumes {
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "6G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
