5ca0843442b320be98269bf89710fb78f13ffb94
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-26 05:20:49 -0400

    bingle: Migrate to disko

--- hosts/bingle/hardware-configuration.nix
+++ hosts/bingle/hardware-configuration.nix
@@ -23,50 +23,5 @@
     extraModulePackages = [ ];
   };
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/05386018-e3b7-46e0-85c9-6539d349fc88";
-      fsType = "btrfs";
-      options = [
-        "subvol=root"
-        "compress=zstd"
-        "noatime"
-      ];
-    };
-
-    "/home" = {
-      device = "/dev/disk/by-uuid/05386018-e3b7-46e0-85c9-6539d349fc88";
-      fsType = "btrfs";
-      options = [
-        "subvol=home"
-        "compress=zstd"
-        "noatime"
-      ];
-    };
-
-    "/nix" = {
-      device = "/dev/disk/by-uuid/05386018-e3b7-46e0-85c9-6539d349fc88";
-      fsType = "btrfs";
-      options = [
-        "subvol=nix"
-        "compress=zstd"
-        "noatime"
-      ];
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/1142-855E";
-      fsType = "vfat";
-      options = [
-        "fmask=0077"
-        "dmask=0077"
-      ];
-    };
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/f0d45e69-9e59-4387-8c9b-a9e45b4dd4a4"; }
-  ];
-
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
