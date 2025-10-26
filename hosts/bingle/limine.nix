4464f142a86294916191134bc025210c51792f48
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-26 15:03:02 -0400

    bingle/limine: Ready secure boot

--- hosts/bingle/limine.nix
+++ hosts/bingle/limine.nix
@@ -1,11 +1,18 @@
+{ pkgs, ... }:
+
 {
+  environment.systemPackages = [ pkgs.sbctl ];
+
   catppuccin = {
     limine.enable = true;
     tty.enable = true;
   };
 
   boot.loader = {
-    limine.enable = true;
+    limine = {
+      enable = true;
+      secureBoot.enable = false;
+    };
     efi.canTouchEfiVariables = true;
   };
 }
