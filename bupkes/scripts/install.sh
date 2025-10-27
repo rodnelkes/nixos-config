6a8a37483df335a8b96507e9320602e2061b326f
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-27 02:04:00 -0400

    nixos-config: Create remote install script

---
+++ bupkes/scripts/install.sh
@@ -0,0 +1,28 @@
+#!/run/current-system/sw/bin/bash
+
+cd ~/nixos-config/
+
+ip=$2
+hostname=$1
+
+temp=$(mktemp -d)
+
+cleanup() {
+  rm -rf "${temp}"
+}
+trap cleanup EXIT
+
+install -d -m755 "${temp}/etc/ssh"
+cd ./bupkes/secrets/
+
+for type in ed25519 rsa; do
+  output_path=${temp}/etc/ssh/ssh_host_${type}_key
+
+  agenix -d installation_key.age > ${output_path}
+  chmod 600 "${output_path}"
+done
+
+cd ../../
+
+build_host=$(nom-build -A ${hostname}.config.system.build.diskoScript -A ${hostname}.config.system.build.toplevel --no-out-link)
+nix run github:nix-community/nixos-anywhere -- --extra-files "${temp}" --store-paths ${build_host} root@${ip}
