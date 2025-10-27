e5806d543b75fe536c30ac022c9f73578938d3e0
Zayen Yusuf <rodnelkes@gmail.com>
2025-10-27 19:13:35 -0400

    bupkes/scripts/install.sh: Rewrite with lsp and formatter suggestions

--- bupkes/scripts/install.sh
+++ bupkes/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
-cd ~/nixos-config/
+cd ~/nixos-config/ || exit
 
 ip=$2
 hostname=$1
@@ -8,21 +8,22 @@ hostname=$1
 temp=$(mktemp -d)
 
 cleanup() {
-  rm -rf "${temp}"
+    rm -rf "${temp}"
 }
 trap cleanup EXIT
 
 install -d -m755 "${temp}/etc/ssh"
-cd ./bupkes/secrets/
+cd ./bupkes/secrets/ || exit
 
 for type in ed25519 rsa; do
-  output_path=${temp}/etc/ssh/ssh_host_${type}_key
+    output_path=${temp}/etc/ssh/ssh_host_${type}_key
 
-  agenix -d installation_key.age > ${output_path}
-  chmod 600 "${output_path}"
+    agenix -d installation_key.age >"${output_path}"
+    chmod 600 "${output_path}"
 done
 
 cd ../../
 
-build_host=$(nom-build -A ${hostname}.config.system.build.diskoScript -A ${hostname}.config.system.build.toplevel --no-out-link)
-nix run github:nix-community/nixos-anywhere -- --extra-files "${temp}" --store-paths ${build_host} root@${ip}
+build_host=$(nom-build -A "${hostname}".config.system.build.diskoScript -A "${hostname}".config.system.build.toplevel --no-out-link)
+# shellcheck disable=SC2086
+nix run github:nix-community/nixos-anywhere -- --extra-files "${temp}" --store-paths ${build_host} root@"${ip}"
