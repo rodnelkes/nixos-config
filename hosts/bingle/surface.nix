c791c0aee06fe329a22f1104afadeaad7e0c620a
Zayen Yusuf <rodnelkes@gmail.com>
2026-02-03 22:35:44 -0500

    hosts/bingle: Use PR branch to fix building surface-kernel

--- hosts/bingle/surface.nix
+++ hosts/bingle/surface.nix
@@ -1,7 +1,10 @@
-{ sources, ... }:
+{ sources, pkgs, ... }:
 
 {
   imports = [ (import "${sources.nixos-hardware}/microsoft/surface/surface-pro-intel") ];
 
   hardware.microsoft-surface.kernelVersion = "stable";
+
+  # zfs_2_3 is broken on unstable
+  boot.zfs.package = pkgs.zfs_2_4;
 }
