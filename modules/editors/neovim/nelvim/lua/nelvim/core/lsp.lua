034062b87a82aab79f155a3b2f029b514d68c973
Zayen Yusuf <rodnelkes@gmail.com>
2026-02-18 17:41:12 -0500

    nelvim: Add nu lsp and formatter

--- modules/editors/neovim/nelvim/lua/nelvim/core/lsp.lua
+++ modules/editors/neovim/nelvim/lua/nelvim/core/lsp.lua
@@ -2,7 +2,7 @@ vim.lsp.config("*", {
 	root_markers = { ".git", ".jj" },
 })
 
-vim.lsp.enable({ "bashls", "kdl-lsp", "lua_ls", "nixd" })
+vim.lsp.enable({ "bashls", "kdl-lsp", "lua_ls", "nixd", "nu-lsp" })
 
 vim.diagnostic.config({
 	virtual_lines = true,
