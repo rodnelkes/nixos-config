5b3d54eda1a9b2eb100d00e6b5a3ff9cd29be93e
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-09 20:31:12 -0400

    wrappers/wezterm: Disable window_decorations

--- wrappers/config/wezterm/wezterm.lua
+++ wrappers/config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 
 config.enable_wayland = false
 
+config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Catppuccin Mocha"
