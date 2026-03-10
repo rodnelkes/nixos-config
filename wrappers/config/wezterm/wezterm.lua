f5d7730ae16e8e52864a9a7f9774270290ca73ae
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-09 20:22:50 -0400

    wrappers/wezterm: Disable wayland support

--- wrappers/config/wezterm/wezterm.lua
+++ wrappers/config/wezterm/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.enable_wayland = false
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Catppuccin Mocha"
