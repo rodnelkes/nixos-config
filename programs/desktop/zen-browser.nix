b282cfb23722d4d9b1f42e9b8d1e877ad64816c2
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-11 00:03:37 -0400

    programs/zen-browser: Setup default policies

--- programs/desktop/zen-browser.nix
+++ programs/desktop/zen-browser.nix
@@ -9,10 +9,29 @@
 let
   inherit (lib) mkIf;
 
-  zen-browser = (import sources.zen-browser) { inherit pkgs; };
+  zen-browser = ((import sources.zen-browser) { inherit pkgs; }).twilight.override {
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
 in
 {
-  environment.systemPackages = [ zen-browser.twilight ];
+  environment.systemPackages = [ zen-browser ];
 
   persist.user.directories = mkIf bupkes.host.features.impermanence [
     ".cache/zen"
