2a8d08290b2ae46d961a328246aece9b19dfb545
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-13 03:18:22 -0400

    programs/desktop/discord: Persist vesktop config

--- programs/desktop/discord.nix
+++ programs/desktop/discord.nix
@@ -1,5 +1,13 @@
-{ bupkes, ... }:
-
+{ lib, bupkes, ... }:
+let
+  inherit (lib) mkIf;
+in
 {
-  environment.systemPackages = [ bupkes.wrappers.vesktop.drv ];
+  environment = {
+    systemPackages = [ bupkes.wrappers.vesktop.drv ];
+
+    persistence."/persistent".users.${bupkes.user.username}.directories =
+      mkIf bupkes.host.features.impermanence
+        [ ".config/vesktop" ];
+  };
 }
