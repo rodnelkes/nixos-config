81addff1a1825549e3efc81dbb6a046b8b323a31
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-13 02:36:58 -0400

    programs/desktop: Add vesktop

---
+++ wrappers/modules/vesktop.nix
@@ -0,0 +1,24 @@
+_:
+
+{
+  name = "vesktop";
+
+  inputs = {
+    nixpkgs.path = "/nixpkgs";
+  };
+
+  impl =
+    { inputs }:
+    let
+      inherit (inputs.nixpkgs.pkgs) symlinkJoin vesktop makeWrapper;
+    in
+    symlinkJoin {
+      name = "vesktop-wrapped";
+      paths = [ vesktop ];
+      buildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/vesktop
+      '';
+      meta.mainProgram = "vesktop";
+    };
+}
