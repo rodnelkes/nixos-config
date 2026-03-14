6c62a94c88a623b5f7c3a4f8a5b7a3bf7be6e1e3
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-13 21:35:15 -0400

    Revert "wrappers/wezterm: Disable window_decorations"
    This reverts commit 5b3d54eda1a9b2eb100d00e6b5a3ff9cd29be93e.

--- wrappers/config/wezterm/wezterm.lua
+++ wrappers/config/wezterm/wezterm.lua
@@ -4,7 +4,6 @@ local config = wezterm.config_builder()
 
 config.enable_wayland = false
 
-config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Catppuccin Mocha"
