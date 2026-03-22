8d1a0f7831fd43fffc276e6ce268e49881b43be6
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-22 02:58:03 -0400

    programs/desktop: Enable plymouth

---
+++ programs/desktop/plymouth.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  boot.plymouth = {
+    enable = true;
+    theme = "catppuccin-mocha";
+    themePackages = [ (pkgs.catppuccin-plymouth.override { variant = "mocha"; }) ];
+  };
+}
