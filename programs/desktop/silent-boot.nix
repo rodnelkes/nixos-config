9af051bdba2807bf4307cbe00c31ae651975b742
Zayen Yusuf <rodnelkes@gmail.com>
2026-03-22 02:39:07 -0400

    programs/desktop: Enable silent boot

---
+++ programs/desktop/silent-boot.nix
@@ -0,0 +1,15 @@
+{
+  boot = {
+    consoleLogLevel = 3;
+
+    initrd.verbose = false;
+
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+
+    loader.timeout = 0;
+  };
+}
