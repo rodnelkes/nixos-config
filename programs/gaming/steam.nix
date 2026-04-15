66900d5de56b89b4aabe0114a96a846409bb38f1
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-15 19:35:37 -0400

    gaming: Add steam

---
+++ programs/gaming/steam.nix
@@ -0,0 +1,21 @@
+{ lib, bupkes, ... }:
+let
+  inherit (lib) mkIf;
+in
+{
+  programs.steam.enable = true;
+
+  environment.persistence."/persistent".users.${bupkes.user.username} =
+    mkIf bupkes.host.features.impermanence
+      {
+        directories = [
+          ".local/share/Steam"
+          ".steam"
+        ];
+
+        files = [
+          ".steampath"
+          ".steampid"
+        ];
+      };
+}
