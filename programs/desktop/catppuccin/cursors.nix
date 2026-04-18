4d12bc1df137cd76d0b36e9a1b09665cdbff71f2
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-18 18:56:42 -0400

    programs/desktop/catppuccin/cursors: Fix for x11 apps

--- programs/desktop/catppuccin/cursors.nix
+++ programs/desktop/catppuccin/cursors.nix
@@ -4,4 +4,9 @@
   environment.systemPackages = with pkgs; [
     catppuccin-cursors.mochaDark
   ];
+
+  hj.files.".icons/default/index.theme".text = ''
+    [Icon Theme]
+    Inherits=catppuccin-mocha-dark-cursors
+  '';
 }
