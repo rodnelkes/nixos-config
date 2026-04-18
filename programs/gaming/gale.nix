089be44780b33399e56b96c6d4ca9c2e423f232c
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-17 20:05:35 -0400

    programs/gaming/gale: Add check for impermanence

--- programs/gaming/gale.nix
+++ programs/gaming/gale.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }:
-
+{ pkgs, lib, bupkes, ... }:
+let
+  inherit (lib) mkIf;
+in
 {
   environment.systemPackages = [ pkgs.gale ];
 
-  persist.user.directories = [
+  persist.user.directories = mkIf bupkes.host.features.impermanence [
     ".cache/com.kesomannen.gale"
     ".config/com.kesomannen.gale"
     ".local/share/com.kesomannen.gale"
