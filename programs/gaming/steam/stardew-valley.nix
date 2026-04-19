77d0c1d575ecbfbc9541079a9a999b8abf564ad3
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-19 15:25:58 -0400

    programs/gaming/steam/stardew-valley: Document fix for multiplayer

--- programs/gaming/steam/stardew-valley.nix
+++ programs/gaming/steam/stardew-valley.nix
@@ -4,4 +4,10 @@ let
 in
 {
   persist.user.directories = mkIf bupkes.host.features.impermanence [ ".config/StardewValley" ];
+
+  # This fixes multiplayer when running on native linux:
+  # http://forums.stardewvalley.net/threads/galaxy-api-not-loading-with-glibc-2-41.36974/post-173903
+  # nix-shell -p prelink
+  # cd `~/.steam/steam/steamapps/common/Stardew Valley/`
+  # execstack -c libGalaxy64.so && execstack -c libGalaxyCSharpGlue.so
 }
