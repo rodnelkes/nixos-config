0cd7c905b4be5959909d77593120224948d58a66
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-20 15:34:07 -0400

    hosts/amende/jellyfin: Persist program data

--- hosts/amende/jellyfin.nix
+++ hosts/amende/jellyfin.nix
@@ -1,6 +1,12 @@
+{ lib, bupkes, ... }:
+let
+  inherit (lib) mkIf;
+in
 {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
+
+  persist.system.directories = mkIf bupkes.host.features.impermanence [ "/var/lib/jellyfin" ];
 }
