5c3559be527b1784966a38335024a6446313e26a
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-21 21:22:48 -0400

    agenix: Rename system to host

--- bupkes/secrets/secrets.nix
+++ bupkes/secrets/secrets.nix
@@ -9,7 +9,7 @@ let
     "rodnelkes@bingle" =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOnFR3Mogjp+bnWYmCD/KujxtqghlXiKBXI1qsLx+Q8 rodnelkes@bingle";
   };
-  systems = {
+  hosts = {
     amende = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILE+GmyhjSsejky9iuZ3UFXIhHgplzvYH7XRDC6bJmao root@amende";
     boobookeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtDEDKhmXloobbC2FRZgfbfVREU94CJn75JFv8eJlMG root@boobookeys";
     bingle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWDQ+59qh8Y8imkqj3pWdel5miFlVJUhrnccnl9tj70 root@bingle";
@@ -17,8 +17,8 @@ let
   installationKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqDOSRs002nj3Bhe5vWdi//sEBUyF6J4XiQB2Hk5K2u installation";
 
   userKeys = attrValues users;
-  systemKeys = attrValues systems;
-  allKeys = userKeys ++ systemKeys ++ [ installationKey ];
+  hostKeys = attrValues hosts;
+  allKeys = userKeys ++ hostKeys ++ [ installationKey ];
 in
 {
   "user_password.age".publicKeys = allKeys;
