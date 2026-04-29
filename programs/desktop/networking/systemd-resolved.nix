782ad2f63ba1f1f752867672fcf122b3a7b9e29c
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-28 21:13:31 -0400

    programs/desktop/systemd-resolved: Cleanup comments

--- programs/desktop/networking/systemd-resolved.nix
+++ programs/desktop/networking/systemd-resolved.nix
@@ -1,13 +1,6 @@
 {
   services.resolved = {
     enable = true;
-    settings.Resolve = {
-      # DNS = [
-      #   "10.2.0.1"
-      #   "2a07:b944::2:1"
-      # ];
-      # Domains = [ "~." ];
-      FallbackDNS = null;
-    };
+    settings.Resolve.FallbackDNS = null;
   };
 }
