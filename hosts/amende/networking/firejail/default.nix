ea54275e933ba05f89cef6997c4213e08f844529
Zayen Yusuf <rodnelkes@gmail.com>
2026-04-29 17:55:18 -0400

    nixos-config: Refactor firejail Move it to amende, because it's the only one that uses network namespaces. Creates a new shell script that runs any application under the vpn, but with conditional profiles starting with zen-twilight. Change niri macro command back to zen-twilight.

---
+++ hosts/amende/networking/firejail/default.nix
@@ -0,0 +1,52 @@
+{
+  sources,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (pkgs) writeShellScriptBin firejail;
+  inherit (lib) getExe;
+
+  zen-browser = (import sources.zen-browser) { inherit pkgs; };
+  zen-twilight = zen-browser.twilight;
+
+  ns = "protonvpn";
+  vpn = writeShellScriptBin "vpn" ''
+    profile="--noprofile"
+    run="''${@}"
+
+    if [[ "''${1}" == "zen-twilight" ]]; then
+        shift
+
+        profile="--profile="${firejail}/etc/firejail/zen-browser.profile""
+        run="${getExe zen-twilight} -P ${ns} "''${@}""
+    fi
+    exec firejail "''${profile}" \
+          --netns=${ns} \
+          --dns=10.2.0.1 \
+          --dns=2a07:b944::2:1 \
+          ''${run}
+  '';
+in
+{
+  programs.firejail.enable = true;
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      firejail = prev.firejail.overrideAttrs (
+        finalAttrs: prevAttrs: {
+          version = "0.9.80";
+          src = sources.firejail.outPath;
+
+          patches = [
+            ./fbuilder-call-firejail-on-path.patch
+            ./use-config-folder-zen-browser.patch
+          ];
+        }
+      );
+    })
+  ];
+
+  environment.systemPackages = [ vpn ];
+}
--- programs/desktop/zen-browser.nix
+++ programs/desktop/zen-browser.nix
@@ -3,47 +3,17 @@
   pkgs,
   lib,
   bupkes,
-  config,
   ...
 }:
 
 let
-  inherit (pkgs) iproute2 firejail writeShellScriptBin;
-  inherit (lib) getExe' getExe mkIf;
-
-  ip = getExe' iproute2 "ip";
-  ns = "protonvpn";
+  inherit (lib) mkIf;
 
   zen-browser = (import sources.zen-browser) { inherit pkgs; };
   zen-twilight = zen-browser.twilight;
-
-  shellScript = (
-    writeShellScriptBin "zen" ''
-      if [[ $(${ip} netns identify) == ${ns} ]]; then
-          exec zen-twilight-${ns} "$@"
-      else
-          exec zen-twilight -P "Default Profile" "$@"
-      fi
-    ''
-  );
 in
 {
-  programs.firejail.wrappedBinaries = mkIf config.programs.firejail.enable {
-    "zen-twilight-${ns}" = {
-      profile = "${firejail}/etc/firejail/zen-browser.profile";
-      executable = "${getExe zen-twilight} -P ${ns}";
-      extraArgs = [
-        "--netns=${ns}"
-        "--dns=10.2.0.1"
-        "--dns=2a07:b944::2:1"
-      ];
-    };
-  };
-
-  environment.systemPackages = [
-    zen-twilight
-    shellScript
-  ];
+  environment.systemPackages = [ zen-twilight ];
 
   persist.user.directories = mkIf bupkes.host.features.impermanence [
     ".cache/zen"
