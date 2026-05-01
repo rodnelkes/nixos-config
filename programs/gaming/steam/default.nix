c98357ca83f486430509c1040ed45d0be7e99d10
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-01 09:01:16 -0400

    programs/gaming/steam: Change steam icon fixer script to a systemd service

--- programs/gaming/steam/default.nix
+++ programs/gaming/steam/default.nix
@@ -5,37 +5,10 @@
   ...
 }:
 let
-  inherit (pkgs) writeShellScriptBin;
+  inherit (pkgs) gnugrep coreutils;
   inherit (lib) mkIf;
-
-  iconFixScript = writeShellScriptBin "steam-icon-fixer" ''
-    for file in ${bupkes.user.homeDirectory}/.local/share/applications/*.desktop; do
-        # Checks if entry is for a steam game
-        grep -q "Comment=Play this game on Steam" "''${file}"
-        isSteamGame=$?
-        if [[ $isSteamGame -eq 1 ]]; then
-            continue
-        fi
-
-        # Checks if the entry is already patched
-        grep -q "StartupWMClass" "''${file}"
-        isPatched=$?
-        if [[ $isPatched -eq 0 ]]; then
-            continue
-        fi
-
-        # Generates the patch line
-        gameId=$(grep -Po "steam://rungameid/\\K\\d+" "''${file}")
-        patchLine="StartupWMClass=steam_app_$gameId"
-
-        # Patches the entry
-        echo $patchLine >> "''${file}"
-    done
-  '';
 in
 {
-  environment.systemPackages = [ iconFixScript ];
-
   programs = {
     steam = {
       enable = true;
@@ -44,6 +17,52 @@ in
     };
 
     gamemode.enable = true;
+  };
+
+  systemd.services.steam-icon-fixer = {
+    enable = true;
+    description = "steam desktop icon fixer";
+    before = [ "graphical.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = false;
+
+      User = bupkes.user.username;
+    };
+
+    path = [
+      gnugrep
+      coreutils
+    ];
+
+    script = ''
+      set +e
+
+      for file in ${bupkes.user.homeDirectory}/.local/share/applications/*.desktop; do
+          # Checks if entry is for a steam game
+          grep -q "Comment=Play this game on Steam" "''${file}"
+          isSteamGame=$?
+          if [[ $isSteamGame -eq 1 ]]; then
+              continue
+          fi
+
+          # Checks if the entry is already patched
+          grep -q "StartupWMClass" "''${file}"
+          isPatched=$?
+          if [[ $isPatched -eq 0 ]]; then
+              continue
+          fi
+
+          # Generates the patch line
+          gameId=$(grep -Po "steam://rungameid/\\K\\d+" "''${file}")
+          patchLine="StartupWMClass=steam_app_$gameId"
+
+          # Patches the entry
+          echo $patchLine >> "''${file}"
+      done
+    '';
   };
 
   persist.user = mkIf bupkes.host.features.impermanence {
