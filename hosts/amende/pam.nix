b6a47d986877772bfad23d87de14f4ddf845f6f7
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-02 01:58:55 -0400

    hosts/amende/pam: Disable for u2f for ly And cue for key

--- hosts/amende/pam.nix
+++ hosts/amende/pam.nix
@@ -3,10 +3,15 @@ let
   inherit (lib) mkIf;
 in
 {
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  security.pam = {
+    u2f = {
+      enable = true;
+      settings.cue = true;
+    };
+
+    services.ly.u2f.enable = false;
   };
 
+  # For some reason, the authfile as an agenix secret doesn't work.
   persist.user.directories = mkIf bupkes.host.features.impermanence [ ".config/Yubico" ];
 }
