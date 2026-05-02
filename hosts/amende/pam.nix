2740e5fc79d151ad7aced8f8ee64fa0c41f96af9
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-02 00:26:47 -0400

    hosts/amende: Use Yubikey to login

--- hosts/amende/pam.nix
+++ hosts/amende/pam.nix
@@ -3,5 +3,10 @@ let
   inherit (lib) mkIf;
 in
 {
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+
   persist.user.directories = mkIf bupkes.host.features.impermanence [ ".config/Yubico" ];
 }
