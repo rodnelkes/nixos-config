8f327f0ea7f046a50f4b2870f629d8ed1882a0a7
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-03 19:20:43 -0400

    hosts/amende/qbittorrent: Configure qBittorrent

--- hosts/amende/qbittorrent.nix
+++ hosts/amende/qbittorrent.nix
@@ -1,3 +1,4 @@
+{ bupkes, ... }:
 let
   ns = "protonvpn";
 in
@@ -5,6 +6,26 @@ in
   services = {
     qbittorrent = {
       enable = true;
+
+      serverConfig = {
+        BitTorrent.Session = {
+          DefaultSavePath = "/mnt/sda1/qBittorrent";
+          Interface = "wg0";
+          InterfaceName = "wg0";
+          InterfaceAddress = "10.2.0.2";
+          DisableAutoTMMByDefault = false;
+        };
+
+        Preferences = {
+          WebUI = {
+            Username = bupkes.user.username;
+            Password_PBKDF2 = "\"@ByteArray(gtAY42yCLgWcy08T26d4ew==:QaOfneMfe1OYW5WGz6QQBAdN/ulVYnLtDcFX+BzzFUUXKs6YKdZ6NADxJ4uQUAXuAXbBqyxHkQCSqKiMC1+Zrg==)\"";
+          };
+          Advanced.RecheckOnCompletion = true;
+        };
+
+        Network.PortForwardingEnabled = false;
+      };
     };
 
     nginx.virtualHosts."rod.nelk.es".locations."/qbittorrent/".proxyPass = "http://192.168.99.2:8080/";
