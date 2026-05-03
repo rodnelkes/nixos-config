e4e7ee95116c59ad6ea1d2398b1006393377147c
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-03 18:59:45 -0400

    system/lix: Use lix package from nixpkgs

--- system/lix.nix
+++ system/lix.nix
@@ -1,30 +1,7 @@
-{ sources, ... }:
-
+{ pkgs, ... }:
 let
-  inherit (builtins) substring;
-
-  # This is only needed because (import sources.lix).lastModifiedDate is "19700101000000"
-  # Not sure why, maybe fault lies with npins?
-  lixInfo =
-    if (import sources.lix).lastModifiedDate == "19700101000000" then
-      let
-        lixRepo = sources.lix.repository;
-        lixUrl = "${lixRepo.server}/${lixRepo.owner}/${lixRepo.repo}";
-      in
-      fetchGit {
-        url = lixUrl;
-        ref = sources.lix.branch;
-        rev = sources.lix.revision;
-      }
-    else
-      (import sources.lix) // { shortRev = substring 0 7 sources.lix.revision; };
+  inherit (pkgs.lixPackageSets.git) lix;
 in
 {
-  imports = [
-    (import "${sources.lix-nixos-module}/module.nix" {
-      lix = sources.lix.outPath;
-
-      versionSuffix = "pre${substring 0 8 lixInfo.lastModifiedDate}-${lixInfo.shortRev}";
-    })
-  ];
+  nix.package = lix;
 }
