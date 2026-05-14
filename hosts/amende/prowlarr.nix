d3f28f648fb456473cf39f3f7094de326bccafa0
Zayen Yusuf <rodnelkes@gmail.com>
2026-05-13 21:31:12 -0400

    hosts/amende: Setup prowlarr

---
+++ hosts/amende/prowlarr.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+let
+  inherit (lib) mkForce;
+
+  ns = "protonvpn";
+in
+{
+  services = {
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+
+      settings.server.urlbase = "/prowlarr";
+    };
+
+    nginx.virtualHosts."rod.nelk.es".locations."/prowlarr/".proxyPass =
+      "http://192.168.99.2:9696/prowlarr/";
+  };
+
+  systemd.services.prowlarr = {
+    bindsTo = [ "netns-${ns}.service" ];
+    after = [ "netns-${ns}.service" ];
+
+    serviceConfig = {
+      # Causes issues with persisted directory
+      DynamicUser = mkForce false;
+      User = "root";
+      Group = "root";
+
+      NetworkNamespacePath = "/var/run/netns/${ns}";
+    };
+  };
+
+  persist.system.directories = [
+    # The default permissions for the folder without persistence
+    {
+      directory = "/var/lib/prowlarr";
+      user = "root";
+      group = "root";
+      mode = "0777";
+    }
+  ];
+}
