bba9db483d4568c3d9856fce9a510900f07dcc3b
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-03 21:46:48 -0400

    packages/firejail: Undo source overlay nixpkgs updated firejail to 0.9.80

--- packages/firejail/default.nix
+++ packages/firejail/default.nix
@@ -1,5 +1,4 @@
 {
-  sources,
   pkgs,
   ...
 }:
@@ -13,9 +12,6 @@ in
     (final: prev: {
       firejail = prev.firejail.overrideAttrs (
         _finalAttrs: _prevAttrs: {
-          version = "0.9.80";
-          src = sources.firejail.outPath;
-
           patches = [
             ./fbuilder-call-firejail-on-path.patch
             ./use-config-folder-zen-browser.patch
