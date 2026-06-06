65f5e4ee573ac6a82ba33497dcca4924ce412555
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-05 21:59:16 -0400

    programs/gaming: Add mgba

---
+++ programs/gaming/mgba.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  lib,
+  bupkes,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+in
+{
+  environment.systemPackages = with pkgs; [
+    mgba
+  ];
+
+  persist.user.directories = mkIf bupkes.host.features.impermanence [ ".config/mgba" ];
+}
