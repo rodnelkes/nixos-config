ceb9c2d6836c8c16255a6d4ffa93fe93a9263519
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-07 14:53:48 -0400

    programs/gaming/minecraft-server: Update Fabric API

--- programs/gaming/minecraft-server.nix
+++ programs/gaming/minecraft-server.nix
@@ -66,8 +66,8 @@ in
 
       symlinks.mods = linkFarmFromDrvs "mods" (attrValues {
         "Fabric API" = fetchurl {
-          url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/E1mjhYMF/fabric-api-0.150.0%2B26.1.2.jar";
-          sha512 = "238c793b720ed21d2d5b564eca88c714cf2188f7b0fb1fd30864660f80901e2b4dad273994b6f77de3c0aa365f930ed8aaccffac49b36c6456b153b52d5d21dc";
+          url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/yALY9gHM/fabric-api-0.151.0%2B26.1.2.jar";
+          sha512 = "d087349842b962414ba89248f9ef7bc75f537848f4d783435de633ddae8924cd50fd9bffc606aae0f1c2c3ed9b4339623244e1fd34c6b9c17f977528d1303cdd";
         };
 
         "Cloth Config API" = fetchurl {
