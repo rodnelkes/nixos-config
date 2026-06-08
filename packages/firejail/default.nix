37523522336b81b601196b5564cef1b550a51634
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-08 05:09:23 -0400

    packages: Separate vpn package from firejail

--- packages/firejail/default.nix
+++ packages/firejail/default.nix
@@ -1,13 +1,4 @@
 {
-  pkgs,
-  ...
-}:
-let
-  inherit (pkgs) writeShellApplication zen-twilight qbittorrent;
-
-  ns = "protonvpn";
-in
-{
   nixpkgs.overlays = [
     (final: prev: {
       firejail = prev.firejail.overrideAttrs (
@@ -17,40 +8,6 @@ in
           ];
         }
       );
-
-      vpn = writeShellApplication {
-        name = "vpn";
-
-        runtimeInputs = [
-          zen-twilight
-          qbittorrent
-        ];
-
-        checkPhase = "";
-
-        text = ''
-          profile="--noprofile"
-          run="''${@}"
-
-          if [[ "''${1}" == "zen-twilight" ]]; then
-              shift
-
-              profile="--profile="${final.firejail}/etc/firejail/zen-browser.profile""
-              run="zen-twilight -P ${ns} "''${@}""
-          elif [[ "''${1}" == "qbittorrent" ]]; then
-              shift
-              source /var/run/wireguard-wg0-natpmp/port
-
-              profile="--profile="${final.firejail}/etc/firejail/qbittorrent.profile""
-              run="qbittorrent --torrenting-port=''${NAT_PORT} "''${@}""
-          fi
-          exec firejail "''${profile}" \
-                --netns=${ns} \
-                --dns=10.2.0.1 \
-                --dns=2a07:b944::2:1 \
-                ''${run}
-        '';
-      };
     })
   ];
 }
