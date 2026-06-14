be64ae7e83f7e05e8597209b450bb7b3cf1be3ed
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-14 04:40:23 -0400

    packages: Remove vpn overlay and place script in privacy modules

--- programs/privacy/firejail.nix
+++ programs/privacy/firejail.nix
@@ -1,12 +1,48 @@
 {
   pkgs,
+  bupkes,
   ...
 }:
 let
-  inherit (pkgs) vpn;
+  inherit (pkgs)
+    writeShellApplication
+    zen-twilight
+    qbittorrent
+    firejail
+    ;
+  inherit (bupkes.host.features) netns;
 in
 {
   programs.firejail.enable = true;
 
-  environment.systemPackages = [ vpn ];
+  environment.systemPackages = [
+    (writeShellApplication {
+      name = "vpn";
+
+      runtimeInputs = [
+        zen-twilight
+        qbittorrent
+      ];
+
+      checkPhase = "";
+
+      text = ''
+        profile="--noprofile"
+        run="''${@}"
+
+        if [[ "''${1}" == "qbittorrent" ]]; then
+            shift
+            source /var/run/wireguard-wg0-natpmp/port
+
+            profile="--profile="${firejail}/etc/firejail/qbittorrent.profile""
+            run="qbittorrent --torrenting-port=''${NAT_PORT} "''${@}""
+        fi
+        exec firejail "''${profile}" \
+              --netns=${netns} \
+              --dns=10.2.0.1 \
+              --dns=2a07:b944::2:1 \
+              ''${run}
+      '';
+    })
+  ];
 }
