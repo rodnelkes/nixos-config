b0915f7d88628a05e11ba73f03dfc9d8b0fb747c
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-17 23:19:10 -0400

    programs/servarr/assertions: More assertions

--- programs/servarr/assertions.nix
+++ programs/servarr/assertions.nix
@@ -4,6 +4,8 @@
 }:
 let
   inherit (builtins) elem;
+  inherit (bupkes.host.features) splitTunneling portForwarding;
+
   hasModule = module: elem module bupkes.host.features.modules;
 in
 {
@@ -11,6 +13,14 @@ in
     {
       assertion = hasModule "privacy";
       message = "servarr modules require privacy modules";
+    }
+    {
+      assertion = splitTunneling;
+      message = "qBittorrent and Prowlarr require network namespaces";
+    }
+    {
+      assertion = portForwarding;
+      message = "qBittorrent requires port forwarding";
     }
   ];
 }
