7ca7579037a18d6d9ae58e14eaa546c4bf690619
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-20 16:11:23 -0400

    hosts/bingle: Reinstall

--- bupkes/secrets/secrets.nix
+++ bupkes/secrets/secrets.nix
@@ -7,12 +7,12 @@ let
     "rodnelkes@boobookeys" =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpTlwHpvrCyOBYGWYKpFM7Q0OYC8bP39gKU4jpK8AWp rodnelkes@boobookeys";
     "rodnelkes@bingle" =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOnFR3Mogjp+bnWYmCD/KujxtqghlXiKBXI1qsLx+Q8 rodnelkes@bingle";
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEF4PuFasEcnH3TePlMPl0k9dkaRWiPh/9BgzC7BLlES rodnelkes@bingle";
   };
   hosts = {
     amende = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILE+GmyhjSsejky9iuZ3UFXIhHgplzvYH7XRDC6bJmao root@amende";
     boobookeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtDEDKhmXloobbC2FRZgfbfVREU94CJn75JFv8eJlMG root@boobookeys";
-    bingle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWDQ+59qh8Y8imkqj3pWdel5miFlVJUhrnccnl9tj70 root@bingle";
+    bingle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFqPanrn4FUbQloNzGd2UMvEs3Yzvy1UHT+yLr5+AMtz root@bingle";
   };
   installationKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqDOSRs002nj3Bhe5vWdi//sEBUyF6J4XiQB2Hk5K2u installation";
 
