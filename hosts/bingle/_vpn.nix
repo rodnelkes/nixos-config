4c3f162b64432ccf8cc93b5507d089e0eec3efe1
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-20 22:53:22 -0400

    hosts/bingle: Reenable vpn with new provider

--- hosts/bingle/_vpn.nix
+++ hosts/bingle/_vpn.nix
@@ -1,26 +1,34 @@
 {
+  bupkes,
+  config,
+  ...
+}:
+
+{
   networking = {
     firewall.allowedUDPPorts = [ 51821 ];
 
     wireguard.interfaces.wg0 = {
       ips = [
-        "10.2.0.2/32"
-        "2a07:b944::2:2/128"
+        "10.150.109.155/32"
+        "fd7d:76ee:e68f:a993:abc2:5a62:1615:1ead/128"
       ];
 
       peers = [
         {
-          publicKey = "iJIw5umGxtrrSIRxVrSF1Ofu5IDphpBpAJOvsrG4FiI=";
+          publicKey = "PyLCXAQT8KkM4T+dUsOQfn+Ub3pGxfGlxkIApuig+hk=";
+          presharedKeyFile = config.age.secrets."wg-${bupkes.host.hostname}-preshared".path;
           allowedIPs = [
             "0.0.0.0/0"
             "::/0"
           ];
           endpoint = "us3.vpn.airdns.org:51820";
-          persistentKeepalive = 25;
+          persistentKeepalive = 15;
         }
       ];
 
       listenPort = 51821;
+      mtu = 1320;
     };
   };
 }
