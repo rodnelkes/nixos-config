190bb7b673ef362d20e6394847f01191788dc065
Zayen Yusuf <rodnelkes@gmail.com>
2026-06-29 00:00:17 -0400

    bupkes/lib/mkFinalBupkes: Simplify bupkes I no longer need to pass bupkes with hostVars into wrappers

--- bupkes/lib/mkFinalBupkes.nix
+++ bupkes/lib/mkFinalBupkes.nix
@@ -5,20 +5,8 @@
 }:
 
 hostVars:
-let
-  # First applies hostVars to bupkes
-  bupkesHostVars = bupkes // {
-    host = bupkes.host // hostVars;
-  };
-
-  # Then applies wrappers to bupkesHostVars...
-  finalBupkes = bupkesHostVars // {
-    wrappers = import ../../wrappers {
-      inherit sources pkgs;
-
-      # ...while also passing bupkesHostVars to wrappers as an arg
-      bupkes = bupkesHostVars;
-    };
-  };
-in
-finalBupkes
+bupkes
+// {
+  host = bupkes.host // hostVars;
+  wrappers = import ../../wrappers { inherit sources pkgs; };
+}
