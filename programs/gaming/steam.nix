ff2a06186930f8ea049a15504b31163fc4dbe3f8
Zayen Yusuf <rodnelkes@gmail.com>
2026-07-16 22:42:58 -0400

    programs/gaming/steam: Remove commented code

--- programs/gaming/steam.nix
+++ programs/gaming/steam.nix
@@ -1,11 +1,9 @@
 {
-  pkgs,
   lib,
   bupkes,
   ...
 }:
 let
-  inherit (pkgs) gnugrep coreutils;
   inherit (lib) mkIf;
 in
 {
@@ -18,52 +16,6 @@ in
 
     gamemode.enable = true;
   };
-
-  # systemd.services.steam-icon-fixer = {
-  #   enable = true;
-  #   description = "steam desktop icon fixer";
-  #   before = [ "graphical.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = false;
-
-  #     User = bupkes.user.username;
-  #   };
-
-  #   path = [
-  #     gnugrep
-  #     coreutils
-  #   ];
-
-  #   script = ''
-  #     set +e
-
-  #     for file in ${bupkes.user.homeDirectory}/.local/share/applications/*.desktop; do
-  #         # Checks if entry is for a steam game
-  #         grep -q "Comment=Play this game on Steam" "''${file}"
-  #         isSteamGame=$?
-  #         if [[ $isSteamGame -eq 1 ]]; then
-  #             continue
-  #         fi
-
-  #         # Checks if the entry is already patched
-  #         grep -q "StartupWMClass" "''${file}"
-  #         isPatched=$?
-  #         if [[ $isPatched -eq 0 ]]; then
-  #             continue
-  #         fi
-
-  #         # Generates the patch line
-  #         gameId=$(grep -Po "steam://rungameid/\\K\\d+" "''${file}")
-  #         patchLine="StartupWMClass=steam_app_$gameId"
-
-  #         # Patches the entry
-  #         echo $patchLine >> "''${file}"
-  #     done
-  #   '';
-  # };
 
   persist.user = mkIf bupkes.host.features.impermanence {
     directories = [
