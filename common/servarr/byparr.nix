f2ab16aba0866eb1358a437261cd77e5bc0f8591
Zayen Yusuf <rodnelkes@gmail.com>
2026-09-02 23:17:53 -0400

    common/servarr/byparr: Add forgotten config

--- common/servarr/byparr.nix
+++ common/servarr/byparr.nix
@@ -3,9 +3,11 @@
 
   virtualisation.oci-containers.containers.byparr = {
     image = "ghcr.io/thephaseless/byparr:cb2a862386e92f141e8aa3b58f8532ef2fc36ed0-amd64";
+    ports = [ "192.168.99.2:8191:8191" ];
     extraOptions = [
       "--network=ns:/var/run/netns/vpn"
       "--dns=10.128.0.1"
+      "--shm-size=512m"
     ];
   };
 
